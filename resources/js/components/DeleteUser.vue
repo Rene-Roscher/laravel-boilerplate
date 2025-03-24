f7c76e1f547739e06c74337bea09e59b950f219e
Rene-Roscher <roscher794@gmail.com>
2025-03-24 03:10:42 +0100

    added profile & deleteUser translations

--- resources/js/components/DeleteUser.vue
+++ resources/js/components/DeleteUser.vue
@@ -44,39 +44,38 @@ const closeModal = () => {
 
 <template>
     <div class="space-y-6">
-        <HeadingSmall title="Delete account" description="Delete your account and all of its resources" />
+        <HeadingSmall :title="__('settings.profile.deleteAccount')" :description="__('settings.profile.deleteAccountDescription')" />
         <div class="space-y-4 rounded-lg border border-red-100 bg-red-50 p-4 dark:border-red-200/10 dark:bg-red-700/10">
             <div class="relative space-y-0.5 text-red-600 dark:text-red-100">
-                <p class="font-medium">Warning</p>
-                <p class="text-sm">Please proceed with caution, this cannot be undone.</p>
+                <p class="font-medium">{{ __('settings.profile.warning') }}</p>
+                <p class="text-sm">{{ __('settings.profile.warningDescription') }}</p>
             </div>
             <Dialog>
                 <DialogTrigger as-child>
-                    <Button variant="destructive">Delete account</Button>
+                    <Button variant="destructive">{{ __('settings.profile.deleteAccountButton') }}</Button>
                 </DialogTrigger>
                 <DialogContent>
                     <form class="space-y-6" @submit="deleteUser">
                         <DialogHeader class="space-y-3">
-                            <DialogTitle>Are you sure you want to delete your account?</DialogTitle>
+                            <DialogTitle>{{ __('settings.profile.areYouSure') }}</DialogTitle>
                             <DialogDescription>
-                                Once your account is deleted, all of its resources and data will also be permanently deleted. Please enter your
-                                password to confirm you would like to permanently delete your account.
+                                {{ __('settings.profile.deleteWarning') }}
                             </DialogDescription>
                         </DialogHeader>
 
                         <div class="grid gap-2">
-                            <Label for="password" class="sr-only">Password</Label>
-                            <Input id="password" type="password" name="password" ref="passwordInput" v-model="form.password" placeholder="Password" />
+                            <Label for="password" class="sr-only">{{ __('settings.profile.password') }}</Label>
+                            <Input id="password" type="password" name="password" ref="passwordInput" v-model="form.password" :placeholder="__('settings.profile.password')" />
                             <InputError :message="form.errors.password" />
                         </div>
 
                         <DialogFooter class="gap-2">
                             <DialogClose as-child>
-                                <Button variant="secondary" @click="closeModal"> Cancel </Button>
+                                <Button variant="secondary" @click="closeModal">{{ __('settings.profile.cancelButton') }}</Button>
                             </DialogClose>
 
                             <Button variant="destructive" :disabled="form.processing">
-                                <button type="submit">Delete account</button>
+                                <button type="submit">{{ __('settings.profile.deleteAccountButton') }}</button>
                             </Button>
                         </DialogFooter>
                     </form>
@@ -85,3 +84,4 @@ const closeModal = () => {
         </div>
     </div>
 </template>
+
