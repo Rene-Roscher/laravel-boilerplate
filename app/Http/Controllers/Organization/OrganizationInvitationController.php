d99d3080d33b539daccaad8c3ebcd7d96bf6ad38
Rene-Roscher <roscher794@gmail.com>
2025-08-18 12:06:33 +0200

    feat: enhance invitation acceptance logic with authorization checks

--- app/Http/Controllers/Organization/OrganizationInvitationController.php
+++ app/Http/Controllers/Organization/OrganizationInvitationController.php
@@ -2,30 +2,19 @@
 
 namespace App\Http\Controllers\Organization;
 
-use App\Enums\OrganizationRoleEnum;
 use App\Http\Controllers\Controller;
-use App\Models\Organization\Organization;
 use App\Models\Organization\OrganizationInvitation;
-use App\Models\User;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Validation\Rule;
-use Inertia\Inertia;
 
 class OrganizationInvitationController extends Controller
 {
-
     public function accept(OrganizationInvitation $invitation)
     {
+        $this->authorize('accept', $invitation);
+
         $user = auth()->user();
         $organization = $invitation->organization;
 
-        if (strtolower($invitation->email) !== strtolower($user->email)) {
-            return redirect()->route('dashboard')->with('error', __('organization.not_authorized_to_accept_invitation'));
-        }
-
-        if (!$organization->users()->where('user_id', $user->id)->exists()) {
+        if (! $organization->users()->where('user_id', $user->id)->exists()) {
             $organization->users()->attach($user->id, [
                 'role' => $invitation->role,
             ]);
@@ -39,5 +28,4 @@ class OrganizationInvitationController extends Controller
 
         return redirect()->route('organization.show', $organization);
     }
-
 }
